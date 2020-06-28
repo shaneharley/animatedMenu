ac7156d33b591d4d45e0bdb17e8f781dc8d772a5
Shane Harley <shaneharley@gmail.com>
2020-06-27 19:30:22 -0700

    Build complete

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
+const menuWrapper = document.querySelector('.menuWrapper')
 const menuHeadings = document.querySelectorAll('.listItems h1')
+const menuNumbers = document.querySelectorAll('.listItems .menuNumber')
+const toggleMenuButton = document.querySelector('#toggleMenuButton')
+const closeMenuIcon = document.querySelector('#closeMenuIcon')
 
-const animateMenuItems = () => {
-  let delay = 0
-  menuHeadings.forEach(menu => {
-    menu.classList.add('active')
-    menu.style.transitionDelay = delay + 's'
+let delay = .5
+
+toggleMenuButton.addEventListener('click', () => {
+  menuWrapper.classList.add('visible')
+  animateMenuItems()
+})
+
+const toggleVisibleClass = (object) => {
+  object.forEach(val => {
+    console.log(delay)
+    val.classList.toggle('visible')
+    val.style.transitionDelay = delay + 's'
     delay = delay + 0.2
   })
+}
+
+const resetStyleOverride = (object) => {
   setTimeout(() => {
-    menuHeadings.forEach(menu => {
-      menu.style.transitionDelay = 0 + 's'
+    object.forEach(val => {
+      val.style = ''
     })
   }, delay)
 }
 
-document.addEventListener('mouseover', () => {
-  animateMenuItems()
+const resetDelay = () => {
+  delay = 0
+}
+
+const animateMenuItems = () => {
+  toggleVisibleClass(menuNumbers)
+  toggleVisibleClass(menuHeadings)
+  resetStyleOverride(menuNumbers)
+  resetStyleOverride(menuHeadings)
+  resetDelay()
+}
+
+menuHeadings.forEach(item => {
+  let number = item.previousElementSibling
+  const toggleHover = () => {
+    item.classList.toggle('hover')
+    number.classList.toggle('hover')
+  }
+  item.addEventListener('mouseover', () => {
+    toggleHover()
+  })
+  item.addEventListener('mouseleave', () => {
+    toggleHover()
+  })
+})
+
+closeMenuIcon.addEventListener('click', () => {
+  menuWrapper.classList.remove('visible')
+  toggleVisibleClass(menuNumbers)
+  toggleVisibleClass(menuHeadings)
+  resetStyleOverride(menuNumbers)
+  resetStyleOverride(menuHeadings)
+  resetDelay()
 })
